65d7de318650ff0bfd74f1eeaabeff4ccb0402eb
Brennon Nuckols <brennuck@gmail.com>
2020-05-24 12:31:46 -0600

    GET /

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,6 +1,17 @@
-const router = require('express').Router();
-const Posts = require('../data/db.js');
+const router = require("express").Router();
+const Posts = require("../data/db.js");
 
-
+router.get("/", (req, res) => {
+  Posts.find(req.query)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "The post with the specified ID does not exist.",
+      });
+    });
+});
 
 module.exports = router;
