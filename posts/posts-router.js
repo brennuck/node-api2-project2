d09c4558ccff4258b742f08cb162e16eed513a6a
Brennon Nuckols <brennuck@gmail.com>
2020-05-24 14:04:17 -0600

    look pretty

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -93,27 +93,27 @@ router.delete("/:id", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
-    const changes = req.body;
-    Posts.update(req.params.id, changes)
-    .then(post => {
-        if (!post) {
-            res.status(404).json({
-                message: "The post with the specified ID does not exist."
-            })
-        } else if (!changes.title || !changes.contents) {
-            res.status(400).json({
-                errorMessage: "Please provide title and contents for the post."
-            })
-        } else {
-            res.status(200).json(post)
-        }
+  const changes = req.body;
+  Posts.update(req.params.id, changes)
+    .then((post) => {
+      if (!post) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist.",
+        });
+      } else if (!changes.title || !changes.contents) {
+        res.status(400).json({
+          errorMessage: "Please provide title and contents for the post.",
+        });
+      } else {
+        res.status(200).json(post);
+      }
     })
-    .catch(error => {
-        console.log(error);
-        res.status(500).json({
-            error: "The post information could not be modified."
-        })
-    }) 
-})
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: "The post information could not be modified.",
+      });
+    });
+});
 
 module.exports = router;
