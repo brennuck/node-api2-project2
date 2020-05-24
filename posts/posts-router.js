5244a41e96fa9e6424f325d8c74854686129d17d
Brennon Nuckols <brennuck@gmail.com>
2020-05-24 12:39:12 -0600

    GET /:id

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -9,9 +9,28 @@ router.get("/", (req, res) => {
     .catch((error) => {
       console.log(error);
       res.status(500).json({
-        message: "The post with the specified ID does not exist.",
+        error: "The posts information could not be retrieved."
       });
     });
 });
+
+router.get("/:id", (req, res) => {
+    Posts.findById(req.params.id)
+    .then(post => {
+        if (post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist."
+            })
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            error: "The post information could not be retrieved."
+        })
+    })
+})
 
 module.exports = router;
